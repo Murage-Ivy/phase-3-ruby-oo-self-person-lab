101131dc8d6219533fb02ec8e869fcbcfc23edbe
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-10-19 22:46:02 +0300

    Completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -46,14 +46,12 @@ class Person
 
   def clean?
     return true if self.hygiene > 7
-
     false
 
   end
 
   def happy?
     return true if self.happiness > 7
-
     false
 
   end
@@ -67,25 +65,20 @@ class Person
 
   def take_bath
     self.hygiene += 4
-
     "♪ Rub-a-dub just relaxing in the tub ♫"
 
   end
 
   def work_out
     self.hygiene -= 3
-
     self.happiness += 2
-
     "♪ another one bites the dust ♫"
 
   end
 
   def call_friend(friend)
     self.happiness += 3
-
     friend.happiness += 3
-
     "Hi #{friend.name}! It's #{self.name}. How are you?"
 
   end
@@ -95,16 +88,12 @@ class Person
 
     when "politics"
       self.happiness -= 2
-
       friend.happiness -= 2
-
       "blah blah partisan blah lobbyist"
 
     when "weather"
       self.happiness += 1
-
       friend.happiness += 1
-      
       "blah blah sun blah rain"
 
     else
